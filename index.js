68b7608ddd0316704f26fc886230a5e0b4ab8b51
IshaanVerma1130 <IshaanVerma1130@users.noreply.github.com>
2020-10-21 02:53:33 +0530

    connection to heroku

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const { body, validationResult } = require('express-validator');
 const app = Express();
 app.use(Express.json());
 
-const sequelize = new Sequelize('doyourbit', process.env.username, process.env.password, {
-    host: process.env.CLEARDB_DATABASE_URL,
+const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
+    host: process.env.host,
     dialect: 'mysql'
 });
 
